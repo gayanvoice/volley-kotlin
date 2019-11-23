0825a657794b02b60c07f577a19f53189bfe34e2
gayankuruppu <example@example.com>
2019-11-23 11:53:24 +0530

    Add NoCache.kt

--- library/src/main/java/com/volley/kotlin/toolbox/NoCache.kt
+++ library/src/main/java/com/volley/kotlin/toolbox/NoCache.kt
@@ -4,9 +4,11 @@ import com.volley.kotlin.Cache
 
 class NoCache: Cache {
     override fun clear() {}
+
     override operator fun get(key: String?): Cache.Entry? {
         return null
     }
+
     override fun put(key: String?, entry: Cache.Entry?) {}
 
     override fun invalidate(key: String?, fullExpire: Boolean?) {}
