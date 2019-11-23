30bde65df81bfc74617e85a558028c419def1e26
gayankuruppu <example@example.com>
2019-11-23 10:45:47 +0530

    Update AdaptedHttpStack.kt

---
+++ library/src/main/java/com/volley/kotlin/toolbox/AdaptedHttpStack.kt
@@ -0,0 +1,57 @@
+package com.volley.kotlin.toolbox
+
+import com.volley.kotlin.AuthFailureError
+import com.volley.kotlin.Header
+import com.volley.kotlin.Request
+import org.apache.http.conn.ConnectTimeoutException
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.util.*
+
+
+@SuppressWarnings("deprecation")
+open class AdaptedHttpStack:BaseHttpStack {
+
+    var mHttpStack: HttpStack? = null
+
+    constructor(httpStack: HttpStack?) {
+        mHttpStack = httpStack
+    }
+
+    @Throws(IOException::class, AuthFailureError::class)
+    override fun executeRequest(request: Request<*>?, additionalHeaders: Map<String?, String?>?): HttpResponse? {
+        val apacheResp: org.apache.http.HttpResponse
+        try {
+            apacheResp = mHttpStack!!.performRequest(request, additionalHeaders)
+        } catch (e: ConnectTimeoutException) {
+            throw SocketTimeoutException(e.message)
+        }
+
+        val statusCode = apacheResp.statusLine.statusCode
+
+        val headers = apacheResp.allHeaders
+        val headerList: MutableList<Header> = ArrayList(headers.size)
+
+        for (header in headers) {
+            headerList.add(Header(header.name, header.value))
+        }
+
+        if (apacheResp.entity == null) {
+            return HttpResponse(statusCode, headerList)
+        }
+
+        val contentLength = apacheResp.entity.contentLength
+
+        if ((contentLength).toLong () != contentLength){
+            throw IOException("Response too large: $contentLength")
+        }
+
+        return HttpResponse(
+            statusCode,
+            headerList,
+            apacheResp.entity.contentLength.toInt(),
+            apacheResp.entity.content
+        )
+    }
+
+}
--- sample/src/main/java/com/volley/app/MainActivity.kt
+++ sample/src/main/java/com/volley/app/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.text)
-        val url = "https://raw.githubusercontent.com/gayanvoice/volley-kotlin/master/data/file.json"
+        val url = "https://raw.githubusercontent.com/gayanvoice/volley-kotlin/master/data/sample.json"
 
         /*
         // Instantiate the cache
